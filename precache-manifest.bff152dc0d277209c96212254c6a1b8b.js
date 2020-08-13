733cd6aa318353a0b573ae16880998ecb68f5441
unknown <ntdvt197@gmail.com>
2020-08-13 11:37:42 +0700

    Updates

---
+++ precache-manifest.bff152dc0d277209c96212254c6a1b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3815d0b47114ff5fee47583c301cf4",
+    "url": "/simple-crud-cart/index.html"
+  },
+  {
+    "revision": "d71208512e031b07db91",
+    "url": "/simple-crud-cart/static/js/2.92cabeaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-crud-cart/static/js/2.92cabeaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02881f293d37726f9229",
+    "url": "/simple-crud-cart/static/js/main.66779cf1.chunk.js"
+  },
+  {
+    "revision": "facf9f6a2988e29815b8",
+    "url": "/simple-crud-cart/static/js/runtime-main.3e67cd74.js"
+  }
+]);
